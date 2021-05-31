e4d92a3b2f70616e40033237a0727f31287f9f7c
Pechayland <burnu@DESKTOP-3VJM0M6>
2021-05-31 15:10:02 +0800

    Added DLK Achivements

--- src/main/java/net/burnutsplus/tnt_and_disparity/procedures/DLKKingEntityDiesProcedure.java
+++ src/main/java/net/burnutsplus/tnt_and_disparity/procedures/DLKKingEntityDiesProcedure.java
@@ -5,14 +5,20 @@ import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.Explosion;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.particles.ParticleTypes;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
 
 import net.burnutsplus.tnt_and_disparity.TntAndDisparityModElements;
 import net.burnutsplus.tnt_and_disparity.TntAndDisparityMod;
 
 import java.util.Map;
+import java.util.Iterator;
 
 @TntAndDisparityModElements.ModElement.Tag
 public class DLKKingEntityDiesProcedure extends TntAndDisparityModElements.ModElement {
@@ -59,6 +65,18 @@ public class DLKKingEntityDiesProcedure extends TntAndDisparityModElements.ModEl
 		}
 		if (world instanceof World && !((World) world).isRemote) {
 			((World) world).createExplosion(null, (int) x, (int) y, (int) z, (float) 80, Explosion.Mode.DESTROY);
+		}
+		if (entity instanceof ServerPlayerEntity) {
+			Advancement _adv = ((MinecraftServer) ((ServerPlayerEntity) entity).server).getAdvancementManager()
+					.getAdvancement(new ResourceLocation("tnt_and_disparity:dlk_in_the_trash"));
+			AdvancementProgress _ap = ((ServerPlayerEntity) entity).getAdvancements().getProgress(_adv);
+			if (!_ap.isDone()) {
+				Iterator _iterator = _ap.getRemaningCriteria().iterator();
+				while (_iterator.hasNext()) {
+					String _criterion = (String) _iterator.next();
+					((ServerPlayerEntity) entity).getAdvancements().grantCriterion(_adv, _criterion);
+				}
+			}
 		}
 	}
 }
